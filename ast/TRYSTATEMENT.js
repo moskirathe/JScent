229ca11ae4dcfe10a4054c5b1e9cf6533d2fd100
z2e0b <lipeisheng19960209@gmail.com>
2020-06-14 18:34:16 -0700

    1

---
+++ ast/TRYSTATEMENT.js
@@ -0,0 +1,20 @@
+const CATCHCLAUSE = require("./CATCHCLAUSE");
+const BLOCKSTATEMENT = require("./BLOCKSTATEMENT");
+export default class TRYSTATEMENT {
+    parse(node) {
+        this.block = new BLOCKSTATEMENT();
+        this.block.parse(node.block);
+        if (node.handler !== null) {
+            this.handler = new CATCHCLAUSE();
+            this.handler.parse(node.handler);
+        } else {
+            this.handler = null
+        }
+        if (node.finalizer !== null) {
+            this.finalizer = new BLOCKSTATEMENT();
+            this.finalizer.parse(node.finalizer);
+        } else {
+            this.finalizer = null
+        }
+    }
+}
