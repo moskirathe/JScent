1721c967929d7fb868f743ac326182cc12eb0379
Aidan Mathewson <aidan.wendel17@gmail.com>
2020-06-15 00:56:32 -0400

    Parse nodes first runthrough finished

--- ast/FORSTATEMENT.js
+++ ast/FORSTATEMENT.js
@@ -12,22 +12,22 @@ const VARIABLEDECLARATION = require("./VARIABLEDECLARATION");
 
 module.exports = class FORSTATEMENT {
     parse(node) {
-        if (node.init.type === "Expression") {
-            this.init = new EXPRESSION();
-            this.init.parse(node.init)
+        if (node.init === null) {
+             this.init = null;
         } else if (node.init.type === "VariableDeclaration") {
             this.init = new VARIABLEDECLARATION();
             this.init.parse(node.init)
         } else {
-            this.init = null;
+             this.init = new EXPRESSION();
+             this.init.parse(node.init)
         }
-        if (node.test.type == "Expression") {
+        if (node["test"].type !== null) {
             this.test = new EXPRESSION();
-            this.test.parse(node.test)
+            this.test.parse(node["test"]);
         } else {
             this.test = null;
         }
-        if (node.update.type == "Expression") {
+        if (node.update.type !== null) {
             this.update = new EXPRESSION();
             this.update.parse(node.update)
         } else {
--- ast/FUNCTIONDECLARATION.js
+++ ast/FUNCTIONDECLARATION.js
@@ -11,18 +11,20 @@ const BLOCKSTATEMENT = require("./BLOCKSTATEMENT");
 
 module.exports = class FUNCTIONDECLARATION {
     parse(node) {
-        this.id = new IDENTIFIER();
-        this.id.parse(node.id);
+        if (node.id !== null) {
+            this.id = new IDENTIFIER();
+            this.id.parse(node.id);
+        } else {
+            this.id = null;
+        }
         this.params = [];
         for (let param of node.params) {
             let temp = new FUNCTIONPARAMETER();
             this.params.push(temp);
             temp.parse(param);
         }
-        if (node.body.type === "BlockStatement") {
-            let temp = new BLOCKSTATEMENT();
-            this.body = temp;
-            temp.parse(node.body);
-        }
+        let temp = new BLOCKSTATEMENT();
+        this.body = temp;
+        temp.parse(node.body);
     }
 }
